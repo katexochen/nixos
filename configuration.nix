9442c44e11c11b896ed831a17192671c665da782
katexochen <49727155+katexochen@users.noreply.github.com>
2022-08-20 20:56:14 +0200

    Add git sl alias

--- configuration.nix
+++ configuration.nix
@@ -154,7 +154,9 @@ in
       enable = true;
       userName = "katexochen";
       userEmail = "49727155+katexochen@users.noreply.github.com";
-      aliases = { };
+      aliases = {
+        sl = "log --pretty=format:'%Cred%h  %Creset%<(40)%s %Cgreen%<(12)%cr %Cblue%an%Creset%C(yellow)%d%Creset' --abbrev-commit";
+      };
       difftastic.enable = true;
       includes = [{
         contents = {
