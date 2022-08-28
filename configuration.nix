6a5ae2762b68ad4ba3a1676c29b788e3908e8a4f
katexochen <49727155+katexochen@users.noreply.github.com>
2022-08-28 21:03:12 +0200

    Configure waybar

--- configuration.nix
+++ configuration.nix
@@ -289,6 +289,9 @@ in
           "XF86AudioLowerVolume" = "exec ${pkgs.pamixer}/bin/pamixer -d 5";
           "XF86Calculator" = "exec ${pkgs.rofi}/bin/rofi -show calc -modi calc -no-show-mathc -no-sort";
         };
+        output = {
+          "*".bg = "/home/katexochen/nixos/wallpaper/pastel.png fill";
+        };
       };
       extraSessionCommands = ''
         export SDL_VIDEODRIVER=wayland
@@ -318,6 +321,78 @@ in
     programs.waybar = {
       enable = true;
       systemd.target = "sway-session.target";
+      settings = {
+        mainBar = {
+          layer = "top";
+          position = "top";
+          height = 30;
+
+          modules-left = [
+            "sway/workspaces"
+            "sway/mode"
+            "custom/media"
+          ];
+          modules-center = [
+            "sway/window"
+          ];
+          modules-right = [
+            "pulseaudio"
+            "network"
+            "cpu"
+            "memory"
+            "sway/language"
+            "clock"
+            "tray"
+          ];
+
+          "clock" = {
+            tooltip-format = "<big>{:%B %Y}</big>\n<tt><small>{calendar}</small></tt>";
+            format-alt = "{:%d.%m.%y}";
+          };
+
+          "cpu" = {
+            interval = 2;
+            format = "{usage}% ";
+            tooltip = false;
+          };
+
+          "memory" = {
+            interval = 2;
+            format = "{}% ";
+          };
+
+          "network" = {
+            interval = 2;
+            format-wifi = "{essid} ({signalStrength}%) ";
+            format-ethernet = "{ipaddr}/{cidr} ";
+            tooltip-format = "{ifname} via {gwaddr} ";
+            format-linked = "{ifname} (No IP) ";
+            format-disconnected = "Disconnected ⚠";
+            format-alt = " {bandwidthDownBytes}  {bandwidthUpBytes}";
+          };
+
+          "pulseaudio" = {
+            scroll-step = 5;
+            format = "{volume}% {icon} {format_source}";
+            format-bluetooth = "{volume}% {icon} {format_source}";
+            format-bluetooth-muted = " {icon} {format_source}";
+            format-muted = " {format_source}";
+            format-source = "{volume}% ";
+            format-source-muted = "";
+            format-icons = {
+              headphone = "";
+              hands-free = "";
+              headset = "";
+              phone = "";
+              portable = "";
+              default = [ "" "" "" ];
+            };
+            # "on-click" = "pavucontrol";
+          };
+
+        };
+      };
+      style = /home/katexochen/nixos/waybar-style.css;
     };
 
   };
