96e24219836c75f34c3d1ef6628e68ca5a16191b
katexochen <49727155+katexochen@users.noreply.github.com>
2022-09-25 18:09:56 +0200

    Update alacritty config

--- configuration.nix
+++ configuration.nix
@@ -456,6 +456,16 @@ in {
       enable = true;
       settings = {
         selection.save_to_clipboard = true;
+        window = {
+          padding = {
+            x = 20;
+            y = 20;
+          };
+          opacity = 0.8;
+        };
+        font = {
+          size = 12;
+        };
       };
     };
 
