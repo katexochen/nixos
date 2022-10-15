c14fc8fabae2e90fd769e88c7dd2f03ae00acd92
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2022-10-15 11:09:49 +0200

    Add git signing
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- home/git.nix
+++ home/git.nix
@@ -1,18 +1,72 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  userName = "Paul Meyer";
+  userEmail = "49727155+katexochen@users.noreply.github.com";
+  siginigKeyPath = "/home/katexochen/.ssh/gitsign.pub";
+in {
+  home.file.".ssh/allowed_signers".text = "* ${builtins.readFile siginigKeyPath}";
+
+  home.file.".config/git/hooks/commit-msg" = {
+    executable = true;
+    text = ''
+      #!/usr/bin/env sh
+      #
+      # An example hook script to check the commit log message.
+      # Called by "git commit" with one argument, the name of the file
+      # that has the commit message.  The hook should exit with non-zero
+      # status after issuing an appropriate message if it wants to stop the
+      # commit.  The hook is allowed to edit the commit message file.
+      #
+      # To enable this hook, rename this file to "commit-msg".
+
+      # Uncomment the below to add a Signed-off-by line to the message.
+      # Doing this in a hook is a bad idea in general, but the prepare-commit-msg
+      # hook is more suited to it.
+      #
+
+      SOB=$(git var GIT_AUTHOR_IDENT | sed -n 's/^\(.*>\).*$/Signed-off-by: \1/p')
+      grep -qs "^$SOB" "$1" || echo -e "\n$SOB" >> "$1"
+
+      # This example catches duplicate Signed-off-by lines.
+
+      test "" = "$(grep '^Signed-off-by: ' "$1" |
+               sort | uniq -c | sed -e '/^[   ]*1[    ]/d')" || {
+              echo >&2 Duplicate Signed-off-by lines.
+              exit 1
+      }
+    '';
+  };
+
   programs.git = {
     enable = true;
-    userName = "katexochen";
-    userEmail = "49727155+katexochen@users.noreply.github.com";
+
+    userName = "${userName}";
+    userEmail = "${userEmail}";
+
+    signing = {
+      signByDefault = true;
+      key = "";
+    };
+
+    extraConfig = {
+      commit.gpgsign = true;
+      tag.gpgsign = true;
+      gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+      user.signingkey = "${siginigKeyPath}";
+      core.hooksPath = "~/.config/git/hooks";
+
+      init.defaultBranch = "main";
+    };
+
     aliases = {
       sl = "log --pretty=format:'%Cred%h  %Creset%<(40)%s %Cgreen%<(12)%cr %Cblue%an%Creset%C(yellow)%d%Creset' --abbrev-commit";
     };
+
+    # TODO: HM next ver
+    # hooks = {
+    #   pre-commit = "commit-msg";
+    # };
+
     difftastic.enable = true;
-    includes = [
-      {
-        contents = {
-          init.defaultBranch = "main";
-        };
-      }
-    ];
   };
 }
