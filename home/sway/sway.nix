8be86bc096019313c7ceca143671bce2f550eb02
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2022-11-06 18:38:26 +0100

    Fix rofi reference
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- home/sway/sway.nix
+++ home/sway/sway.nix
@@ -2,14 +2,17 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  finalPkg = name: "${config.programs.${name}.finalPackage}";
+  finalPkgBin = name: "${finalPkg name}/bin/${name}";
+in {
   wayland.windowManager.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
     systemdIntegration = true;
     config = {
       terminal = "${pkgs.alacritty}/bin/alacritty";
-      menu = "${pkgs.rofi}/bin/rofi -show drun -show-icons -pid";
+      menu = "${finalPkgBin "rofi"} -show drun -show-icons -pid";
       bars = [{command = "${pkgs.waybar}/bin/waybar";}];
 
       modifier = "Mod4";
@@ -110,7 +113,7 @@
         "${mod}+Shift+v" = ''mode "system:  [r]eboot  [p]oweroff  [e]xit"'';
 
         # Shortcuts for applications
-        "${mod}+c" = "exec ${pkgs.rofi}/bin/rofi -show calc -modi calc -no-show-mathc -no-sort";
+        "${mod}+c" = "exec ${finalPkgBin "rofi"} -show calc -modi calc -no-show-mathc -no-sort";
         "${mod}+p" = "exec ${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g- screenshot-$(date +%Y%m%d-%H%M%S).png";
         "${mod}+Shift+p" = "exec ${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g- - | ${pkgs.wl-clipboard}/bin/wl-copy -t image/png";
         "${mod}+i" = "exec ${pkgs.mako}/bin/makoctl dismiss";
@@ -121,7 +124,7 @@
         "XF86AudioMute" = "exec ${pkgs.pamixer}/bin/pamixer -t";
         "XF86AudioRaiseVolume" = "exec ${pkgs.pamixer}/bin/pamixer -i 5";
         "XF86AudioLowerVolume" = "exec ${pkgs.pamixer}/bin/pamixer -d 5";
-        "XF86Calculator" = "exec ${pkgs.rofi}/bin/rofi -show calc -modi calc -no-show-mathc -no-sort";
+        "XF86Calculator" = "exec ${finalPkgBin "rofi"} -show calc -modi calc -no-show-mathc -no-sort";
       };
 
       modes = {
