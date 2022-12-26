e77734853dd600849b8e4d08ddbccea17e7b9183
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2022-12-26 11:47:26 +0100

    bash: add ~/bin to path and hinstory config
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- home/shell/bash.nix
+++ home/shell/bash.nix
@@ -1,17 +1,25 @@
 {pkgs, ...}: {
   programs.bash = {
     enable = true;
+    historyControl = [
+      "ignoredups"
+      "ignorespace"
+      "erasedups"
+    ];
     historyIgnore = [
       "cd"
       "exit"
       "ls"
       "shutdown"
+      "reboot"
+      "poweroff"
     ];
     sessionVariables = {
       EDITOR = "nano";
     };
     initExtra = ''
       export PATH=$PATH:$(go env GOPATH)/bin
+      export PATH=$PATH:/home/katexochen/bin
     '';
     shellAliases = {
       cat = "${pkgs.bat}/bin/bat -pp";
