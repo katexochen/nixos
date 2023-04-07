1758402a6b438154faae4a2edc4a0aa44eccfb53
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-04-07 16:08:11 +0200

    Deactivate Nvidia gpu
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- hosts/nostro/configuration.nix
+++ hosts/nostro/configuration.nix
@@ -16,4 +16,21 @@
 
   networking.hostName = "nostro";
 
+  ##### disable nvidia, very nice battery life.
+  boot.extraModprobeConfig = lib.mkDefault ''
+    blacklist nouveau
+    options nouveau modeset=0
+  '';
+
+  services.udev.extraRules = lib.mkDefault ''
+    # Remove NVIDIA USB xHCI Host Controller devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA USB Type-C UCSI devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA Audio devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA VGA/3D controller devices
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  '';
+  boot.blacklistedKernelModules = lib.mkDefault ["nouveau" "nvidia"];
 }
