af054682be9dfd9fa0629468d666f6da414b9dfb
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-04-26 16:30:40 +0200

    Disable nvidia on nt5
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- hosts/nt5/configuration.nix
+++ hosts/nt5/configuration.nix
@@ -18,7 +18,25 @@
 
   boot.extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
   boot.kernelModules = ["v4l2loopback"];
+
+  # Disable Nvidia stuff completely
   boot.extraModprobeConfig = ''
     options v4l2loopback exclusive_caps=1
+    blacklist nouveau
+    options nouveau modeset=0
+  '';
+  boot.blacklistedKernelModules = [
+    "nouveau"
+    "nvidia"
+  ];
+  services.udev.extraRules = ''
+    # Remove NVIDIA USB xHCI Host Controller devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA USB Type-C UCSI devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA Audio devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA VGA/3D controller devices
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
   '';
 }
