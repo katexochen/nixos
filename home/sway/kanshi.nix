fb92e62b34c4a36d570a3be7c4fde9fc1c112d8c
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-08-20 21:05:36 +0200

    kanish: add home setup
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- home/sway/kanshi.nix
+++ home/sway/kanshi.nix
@@ -1,6 +1,8 @@
 let
   dellp27L = "Dell Inc. DELL P2720D CBHJK53";
   dellp27R = "Dell Inc. DELL P2720D GN2ZS03";
+  dellu27 = "Dell Inc. DELL U2717D J0XYN8AOA5ML";
+  nt14 = "LG Display 0x06ED Unknown";
 in {
   services.kanshi = {
     enable = true;
@@ -15,6 +17,18 @@ in {
           criteria = dellp27R;
           status = "enable";
           position = "2560,0";
+        }
+      ];
+      home_nt14.outputs = [
+        {
+          criteria = dellu27;
+          status = "enable";
+          position = "0,0";
+        }
+        {
+          criteria = nt14;
+          status = "enable";
+          position = "2560,240";
         }
       ];
     };
