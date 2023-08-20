a82ef58cf0db278abfc1a048dcb153a27a371dea
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-08-20 21:36:21 +0200

    git: refactor hook

--- home/git.nix
+++ home/git.nix
@@ -1,40 +1,17 @@
-{pkgs, ...}: let
+{ pkgs, ... }:
+let
   userName = "Paul Meyer";
   userEmail = "49727155+katexochen@users.noreply.github.com";
   #  siginigKeyPath = "/home/katexochen/.ssh/gitsign.pub";
-in {
-  #  home.file.".ssh/allowed_signers".text = "* ${builtins.readFile siginigKeyPath}";
 
-  home.file.".config/git/hooks/commit-msg" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env sh
-      #
-      # An example hook script to check the commit log message.
-      # Called by "git commit" with one argument, the name of the file
-      # that has the commit message.  The hook should exit with non-zero
-      # status after issuing an appropriate message if it wants to stop the
-      # commit.  The hook is allowed to edit the commit message file.
-      #
-      # To enable this hook, rename this file to "commit-msg".
-
-      # Uncomment the below to add a Signed-off-by line to the message.
-      # Doing this in a hook is a bad idea in general, but the prepare-commit-msg
-      # hook is more suited to it.
-      #
-
-      SOB=$(git var GIT_AUTHOR_IDENT | sed -n 's/^\(.*>\).*$/Signed-off-by: \1/p')
-      grep -qs "^$SOB" "$1" || echo -e "\n$SOB" >> "$1"
-
-      # This example catches duplicate Signed-off-by lines.
-
-      test "" = "$(grep '^Signed-off-by: ' "$1" |
-               sort | uniq -c | sed -e '/^[   ]*1[    ]/d')" || {
-              echo >&2 Duplicate Signed-off-by lines.
-              exit 1
-      }
-    '';
+  commit-msg-hook = pkgs.writeShellApplication {
+    name = "commit-msg-hook";
+    runtimeInputs = with pkgs; [ git gnugrep ];
+    text = (builtins.readFile ./commit-msg.sh);
   };
+in
+{
+  #  home.file.".ssh/allowed_signers".text = "* ${builtins.readFile siginigKeyPath}";
 
   programs.git = {
     enable = true;
@@ -53,7 +30,6 @@ in {
       #   gpg.format = "ssh";
       #   gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       #   user.signingkey = "${siginigKeyPath}";
-      core.hooksPath = "~/.config/git/hooks";
 
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
@@ -68,10 +44,9 @@ in {
       add-fork = "!git remote add \${1%/*} https://github.com/\${1} && git fetch \${1%/*}; #"; # $1 is appended to the end, ignoring it with #
     };
 
-    # TODO: HM next ver
-    # hooks = {
-    #   pre-commit = "commit-msg";
-    # };
+    hooks = {
+      commit-msg = "${commit-msg-hook}/bin/commit-msg-hook";
+    };
 
     difftastic.enable = true;
   };
