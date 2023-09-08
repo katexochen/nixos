c30839b08940779a25b8a5536282463cf0e63a60
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-09-08 18:58:36 +0200

    fs-diff: mount subvolume
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- modules/impermanence/fs-diff.sh
+++ modules/impermanence/fs-diff.sh
@@ -3,21 +3,43 @@
 
 set -euo pipefail
 
-OLD_TRANSID=$(sudo btrfs subvolume find-new /mnt/root-blank 9999999)
-OLD_TRANSID=${OLD_TRANSID#transid marker was }
+cleanup=()
 
-sudo btrfs subvolume find-new "/mnt/root" "$OLD_TRANSID" |
+if [[ ! -e "/mnt" ]]; then
+  sudo mkdir -p /mnt
+  cleanup+=("sudo rm -d /mnt")
+fi
+
+if [[ ! $(findmnt /mnt) ]]; then
+  sudo mount -o subvol=/ /dev/mapper/crypted /mnt
+  cleanup+=("sudo umount /mnt")
+fi
+
+oldTransID=$(sudo btrfs subvolume find-new /mnt/root-blank 9999999)
+oldTransID=${oldTransID#transid marker was }
+
+sudo btrfs subvolume find-new "/mnt/root" "${oldTransID}" |
 sed '$d' |
 cut -f17- -d' ' |
 sort |
 uniq |
 while read -r path; do
-  path="/$path"
-  if [ -L "$path" ]; then
+  path="/${path}"
+  if [ -L "${path}" ]; then
     : # The path is a symbolic link, so is probably handled by NixOS already
-  elif [ -d "$path" ]; then
+  elif [ -d "${path}" ]; then
     : # The path is a directory, ignore
   else
-    echo "$path"
+    echo "${path}"
   fi
 done
+
+exitcode=0
+
+# Cleanup in reverse order
+readarray -t <<< "$(printf '%s\n' "${cleanup[@]}" | tac)"
+for cmd in "${MAPFILE[@]}"; do
+  eval "${cmd}" || exitcode=$?
+done
+
+exit "${exitcode}"
