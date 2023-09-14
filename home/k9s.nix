0d2e19d312972980b436d0e33fb1a5cc313e7d36
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-09-14 21:57:45 +0200

    k9s: simplify skin
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- home/k9s.nix
+++ home/k9s.nix
@@ -5,41 +5,23 @@ _: {
       # https://github.com/derailed/k9s/blob/master/skins/transparent.yml
       # Preserve your terminal session background color
       k9s = {
-        body = {
-          bgColor = "default";
-        };
-        promt = {
-          bgColor = "default";
-        };
-        dialog = {
-          bgColor = "default";
-        };
+        body.bgColor = "default";
+        promt.bgColor = "default";
+        dialog.bgColor = "default";
         frame = {
-          crumbs = {
-            bgColor = "default";
-          };
-          title = {
-            bgColor = "default";
-          };
+          crumbs.bgColor = "default";
+          title.bgColor = "default";
         };
         views = {
-          charts = {
-            bgColor = "default";
-          };
+          charts.bgColor = "default";
           table = {
             bgColor = "default";
-            header = {
-              bgColor = "default";
-            };
+            header.bgColor = "default";
           };
-          xray = {
-            bgColor = "default";
-          };
+          xray.bgColor = "default";
           logs = {
             bgColor = "default";
-            indicator = {
-              bgColor = "default";
-            };
+            indicator.bgColor = "default";
           };
         };
       };
