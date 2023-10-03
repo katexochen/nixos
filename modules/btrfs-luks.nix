2ed2c5ac71039a3427a847395f3afef631a43a54
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-10-03 19:42:48 +0200

    disko: btrfs-luks base
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ modules/btrfs-luks.nix
@@ -0,0 +1,63 @@
+{ lib
+, disk ? "/dev/nvm0n1"
+}: {
+  disko.devices = {
+    disk = lib.genAttrs disk (device: {
+      name = lib.replaceStrings [ "/" ] [ "-" ] device;
+      device = device;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "crypted";
+              extraOpenArgs = [ "--allow-discards" ];
+              askPassword = true;
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "@" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@var/log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    });
+  };
+}
