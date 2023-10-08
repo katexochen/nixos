a369409082cb993bd49d51790209e900d21de752
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-10-08 11:10:54 +0200

    impermanence: talyz backup script
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- modules/impermanence/default.nix
+++ modules/impermanence/default.nix
@@ -39,44 +39,34 @@ in
       ];
     };
 
-    # Note `lib.mkBefore` is used instead of `lib.mkAfter` here.
-    boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
-      mkdir -p /mnt
+    # From https://github.com/talyz/presentations/blob/master/impermanence-nixcon-2023/impermanence.org
+    boot.initrd.postDeviceCommands = pkgs.lib.mkAfter ''
+      mkdir /btrfs_tmp
 
-      # We first mount the btrfs root to /mnt
-      # so we can manipulate btrfs subvolumes.
-      mount -o subvol=/ /dev/mapper/crypted /mnt
+      # Mount the actual Btrfs root, where the subvolumes will be located.
+      mount /dev/mapper/crypted /btrfs_tmp
 
-      # While we're tempted to just delete /root and create
-      # a new snapshot from /root-blank, /root is already
-      # populated at this point with a number of subvolumes,
-      # which makes `btrfs subvolume delete` fail.
-      # So, we remove them first.
-      #
-      # /root contains subvolumes:
-      # - /root/var/lib/portables
-      # - /root/var/lib/machines
-      #
-      # I suspect these are related to systemd-nspawn, but
-      # since I don't use it I'm not 100% sure.
-      # Anyhow, deleting these subvolumes hasn't resulted
-      # in any issues so far, except for fairly
-      # benign-looking errors from systemd-tmpfiles.
-      btrfs subvolume list -o /mnt/root |
-      cut -f9 -d' ' |
-      while read subvolume; do
-        echo "deleting /$subvolume subvolume..."
-        btrfs subvolume delete "/mnt/$subvolume"
-      done &&
-      echo "deleting /root subvolume..." &&
-      btrfs subvolume delete /mnt/root
 
-      echo "restoring blank /root subvolume..."
-      btrfs subvolume snapshot /mnt/root-blank /mnt/root
+      if [[ -e /btrfs_tmp/@ ]]; then
+          mkdir -p /btrfs_tmp/old_@s
+          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
+          mv /btrfs_tmp/@ "/btrfs_tmp/old_@s/$timestamp"
+      fi
 
-      # Once we're done rolling back to a blank snapshot,
-      # we can unmount /mnt and continue on the boot process.
-      umount /mnt
+      delete_subvolume_recursively() {
+          IFS=$'\n'
+          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+              delete_subvolume_recursively "/btrfs_tmp/$i"
+          done
+          btrfs subvolume delete "$1"
+      }
+
+      for i in $(find /btrfs_tmp/old_@s/ -maxdepth 1 -mtime +30); do
+          delete_subvolume_recursively "$i"
+      done
+
+      btrfs subvolume create /btrfs_tmp/@
+      umount /btrfs_tmp
     '';
 
     environment.systemPackages = [
