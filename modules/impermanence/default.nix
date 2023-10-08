ec1bc8556f9f31998aca98462fd5afa9101942bd
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-10-08 11:16:52 +0200

    impermanence: always install tools
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- modules/impermanence/default.nix
+++ modules/impermanence/default.nix
@@ -1,8 +1,5 @@
 { config, lib, pkgs, ... }:
-
 let
-  cfg = config.my.modules.impermanence;
-
   fs-diff = pkgs.writeShellApplication {
     name = "fs-diff";
     text = builtins.readFile ./fs-diff.sh;
@@ -14,64 +11,10 @@ let
   };
 in
 {
-  options.my.modules = {
-    impermanence = {
-      enable = lib.mkEnableOption (lib.mdDoc "impermanence");
-    };
-  };
+  imports = [ ./mod.nix ];
 
-  config = lib.mkIf cfg.enable {
-    environment.persistence."/persist" = {
-      hideMounts = true;
-      directories = [
-        "/etc/NetworkManager/system-connections"
-        "/var/lib/bluetooth"
-        "/var/lib/systemd/backlight"
-      ];
-      files = [
-        "/etc/adjtime"
-        "/etc/docker/key.json"
-        "/etc/machine-id"
-        "/var/lib/NetworkManager/NetworkManager.state"
-        "/var/lib/NetworkManager/secret_key"
-        "/var/lib/NetworkManager/seen-bssids"
-        "/var/lib/NetworkManager/timestamps"
-      ];
-    };
-
-    # From https://github.com/talyz/presentations/blob/master/impermanence-nixcon-2023/impermanence.org
-    boot.initrd.postDeviceCommands = pkgs.lib.mkAfter ''
-      mkdir /btrfs_tmp
-
-      # Mount the actual Btrfs root, where the subvolumes will be located.
-      mount /dev/mapper/crypted /btrfs_tmp
-
-
-      if [[ -e /btrfs_tmp/@ ]]; then
-          mkdir -p /btrfs_tmp/old_@s
-          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
-          mv /btrfs_tmp/@ "/btrfs_tmp/old_@s/$timestamp"
-      fi
-
-      delete_subvolume_recursively() {
-          IFS=$'\n'
-          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_tmp/$i"
-          done
-          btrfs subvolume delete "$1"
-      }
-
-      for i in $(find /btrfs_tmp/old_@s/ -maxdepth 1 -mtime +30); do
-          delete_subvolume_recursively "$i"
-      done
-
-      btrfs subvolume create /btrfs_tmp/@
-      umount /btrfs_tmp
-    '';
-
-    environment.systemPackages = [
-      fs-diff
-      persist
-    ];
-  };
+  environment.systemPackages = [
+    fs-diff
+    persist
+  ];
 }
