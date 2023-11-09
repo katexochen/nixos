3b8bcc85972d1174ef894c5b25ff2550de3279c6
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-11-09 08:53:15 +0100

    vscode: add copilot-chat
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- home/desktop/vscode.nix
+++ home/desktop/vscode.nix
@@ -64,6 +64,12 @@
           sha256 = "1hgj3847g3z3rh6s1kfqk0pjs7qv7li3njl6wfgzv65f6hkn27rp";
         }
         {
+          name = "copilot-chat";
+          publisher = "GitHub";
+          version = "0.11.2023110801";
+          sha256 = "sha256-x/ptkpxMXu2ZGjA6i7+6p2LGT6E/U+x81FsErg8+CV4=";
+        }
+        {
           name = "vscode-bazel";
           publisher = "bazelbuild";
           version = "0.7.0";
