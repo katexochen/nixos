b4943cf1fc35b46e4ecff5d2232937d8d0b89b94
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-11-14 07:18:07 +0100

    remote-builder: update ip, maxJobs, speedFactor
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- modules/remote-builder.nix
+++ modules/remote-builder.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 let
   cfg = config.my.modules.remote-bulder;
 in
@@ -21,15 +21,15 @@ in
           system = "x86_64-linux";
           protocol = "ssh-ng";
           supportedFeatures = [ "nixos-test" "big-parallel" "kvm" ];
-          maxJobs = 2;
-          speedFactor = 2;
+          maxJobs = 6;
+          speedFactor = 10;
         }
       ];
     };
 
     programs.ssh.extraConfig = ''
       Host builder
-              HostName 18.193.70.168
+              HostName 52.28.9.128
               ConnectTimeout 2
               IdentitiesOnly yes
               IdentityFile /root/.ssh/remote_builder
