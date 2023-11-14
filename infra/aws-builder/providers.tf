3148f8f9f7f145dcff2f3ff5a6df1e736cda64e4
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-11-14 07:17:04 +0100

    infra: add aws-builder
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ infra/aws-builder/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
