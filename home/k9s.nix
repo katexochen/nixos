9007754f1192c52082742261be721d162e23e154
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-13 20:44:21 +0100

    k9s: refactor config
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- home/k9s.nix
+++ home/k9s.nix
@@ -1,30 +1,29 @@
-_: {
+{ pkgs, ... }:
+let
+  yamlToJson = drv: pkgs.stdenv.mkDerivation {
+    name = "${drv.name}-json";
+    src = drv;
+    nativeBuildInputs = [ pkgs.yq-go ];
+    buildCommand = ''cat $src | yq -o json > $out'';
+  };
+
+  fromYAML = str: builtins.fromJSON (builtins.readFile (yamlToJson str));
+in
+
+{
   programs.k9s = {
     enable = true;
-    skin = {
-      # https://github.com/derailed/k9s/blob/master/skins/transparent.yml
-      # Preserve your terminal session background color
-      k9s = {
-        body.bgColor = "default";
-        promt.bgColor = "default";
-        dialog.bgColor = "default";
-        frame = {
-          crumbs.bgColor = "default";
-          title.bgColor = "default";
-        };
-        views = {
-          charts.bgColor = "default";
-          table = {
-            bgColor = "default";
-            header.bgColor = "default";
-          };
-          xray.bgColor = "default";
-          logs = {
-            bgColor = "default";
-            indicator.bgColor = "default";
-          };
-        };
+    settings.k9s = {
+      ui = {
+        skin = "transparent";
+        logoless = true;
       };
+      skipLatestRevCheck = true;
+      logger.textWrap = true;
     };
+    skins.transparent = fromYAML (pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/derailed/k9s/v${pkgs.k9s.version}/skins/transparent.yaml";
+      hash = "sha256-4+tCRcI5fsSwqqhnNEZiD6LAc6ZW/AaP7KZ0003/XSE=";
+    });
   };
 }
