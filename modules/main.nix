6b3267b6ecadc29e5a1277d39e5f70656a32367d
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-30 15:39:02 +0100

    nix: set wheel group as trusted-users
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- modules/main.nix
+++ modules/main.nix
@@ -32,6 +32,7 @@ in
       settings = {
         experimental-features = [ "nix-command" "flakes" ];
         auto-optimise-store = true;
+        trusted-users = [ "root" "@wheel" ];
       };
     };
 
