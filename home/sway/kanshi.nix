43f3ae048c549b8eee1809bc8c062a17f42ec451
Paul Meyer <katexochen0@gmail.com>
2025-03-27 08:05:19 +0100

    kanshi: add gf setup
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- home/sway/kanshi.nix
+++ home/sway/kanshi.nix
@@ -2,6 +2,7 @@ let
   dellp27L = "Dell Inc. DELL P2720D CBHJK53";
   dellp27R = "Dell Inc. DELL P2720D GN2ZS03";
   dellu27 = "Dell Inc. DELL U2717D J0XYN8AOA5ML";
+  dellaw25 = "Dell Inc. AW2521HFA 7CWTL03";
   p14s = "Lenovo Group Limited 0x403A Unknown";
   l14 = "Lenovo Group Limited L15 U133MM45";
 in
@@ -66,6 +67,21 @@ in
             criteria = p14s;
             status = "enable";
             position = "1920,0";
+          }
+        ];
+      }
+      {
+        profile.name = "gf";
+        profile.outputs = [
+          {
+            criteria = dellaw25;
+            status = "enable";
+            position = "0,0";
+          }
+          {
+            criteria = p14s;
+            status = "enable";
+            position = "0,1080";
           }
         ];
       }
