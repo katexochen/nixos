81ea96282354b8026f226cf320fd061123224cb7
Paul Meyer <katexochen0@gmail.com>
2025-05-15 15:22:07 +0200

    mako: update config
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- home/sway/mako.nix
+++ home/sway/mako.nix
@@ -2,13 +2,11 @@ _: {
   services.mako = {
     # test with notify-send.
     enable = true;
-    defaultTimeout = 4500;
-    backgroundColor = "#2e3440";
-    ignoreTimeout = true;
-    output = "*"; # TODO: test
-    extraConfig = ''
-      [mode=do-not-disturb]
-      invisible=1
-    '';
+    settings = {
+      default-timeout = 4500;
+      background-color = "#2e3440";
+      ignore-timeout = true;
+      "mode=do-not-disturb".invisible = true;
+    };
   };
 }
