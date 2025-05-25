3c07348f9cd2c588988a00d40c6ca4ca9d68b50d
Paul Meyer <katexochen0@gmail.com>
2025-05-25 16:56:51 +0200

    pi: setup adguard
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- hosts/pi/configuration.nix
+++ hosts/pi/configuration.nix
@@ -21,16 +21,60 @@
     gdu
     git
     go
-    gotools
-    nixpkgs-review
-    starship
   ];
 
-  programs.tmux = {
+  programs = {
+    tmux = {
+      enable = true;
+      clock24 = true;
+      terminal = "screen-256color";
+      historyLimit = 10000;
+    };
+    starship.enable = true;
+  };
+
+  services.adguardhome = {
     enable = true;
-    clock24 = true;
-    terminal = "screen-256color";
-    historyLimit = 10000;
+    openFirewall = true;
+    settings = {
+      dns = {
+        upstream_dns = [
+          # quad9
+          "9.9.9.9"
+          "149.112.112.112"
+          "2620:fe::11"
+          "2620:fe::fe:11"
+          # google
+          "8.8.8.8"
+          "8.8.4.4"
+          "2001:4860:4860::8888"
+          "2001:4860:4860::8844"
+          # cloudflare
+          "1.1.1.1"
+          "1.0.0.1"
+          "2606:4700:4700::64"
+          "2606:4700:4700::6400"
+        ];
+        filtering = {
+          protection_enabled = true;
+          filtering_enabled = true;
+          parental_enabled = false;
+          safe_search.enabled = false;
+        };
+      };
+      filters =
+        map
+          (url: {
+            enabled = true;
+            url = url;
+          })
+          [
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt" # AdGuard DNS filter
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # The Big List of Hacked Malware Web Sites
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_59.txt" # AdGuard DNS Popup Hosts filter
+          ];
+    };
   };
 
   virtualisation.docker.enable = true;
@@ -53,17 +97,21 @@
       generic-extlinux-compatible.enable = true;
       timeout = 7;
     };
-    # kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
     initrd.availableKernelModules = [
       "xhci_pci"
       "usbhid"
       "usb_storage"
     ];
-    kernelModules = [ ];
   };
 
-  networking.useDHCP = true;
-  networking.hostName = "pi";
+  networking = {
+    hostName = "pi";
+    useDHCP = true;
+    firewall = {
+      enable = true;
+      allowedUDPPorts = [ 53 ];
+    };
+  };
 
   services.getty.autologinUser = "root";
 
@@ -73,6 +121,8 @@
     options = [ "noatime" ];
   };
   swapDevices = [ ];
+
+  security.sudo.wheelNeedsPassword = false;
 
   hardware.enableRedistributableFirmware = true;
   system.stateVersion = "25.11";
