79c70edbfa8036b6fdda1ae02a9565caeb3cda53
Paul Meyer <katexochen0@gmail.com>
2025-06-08 21:57:00 +0200

    pi: update adguard config
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- hosts/pi/configuration.nix
+++ hosts/pi/configuration.nix
@@ -1,4 +1,4 @@
-_:
+{ config, ... }:
 
 {
 
@@ -31,30 +31,46 @@ _:
   services.adguardhome = {
     enable = true;
     openFirewall = true;
+    host = "0.0.0.0";
+    port = 3000;
     settings = {
+      schema_version = 29;
+      clients.runtime_sources = {
+        whois = true;
+        arp = true;
+        rdns = true;
+        dhcp = true;
+        hosts = false;
+      };
       dns = {
+        port = 53;
+        bind_hosts = [ "0.0.0.0" ];
+        bootstrap_dns = [ "1.1.1.1" ];
+        local_ptr_upstreams = [ "192.168.178.1" ];
         upstream_dns = [
-          # quad9
           "9.9.9.9"
           "149.112.112.112"
           "2620:fe::11"
           "2620:fe::fe:11"
-          # google
+          "https://dns10.quad9.net/dns-query"
           "8.8.8.8"
           "8.8.4.4"
           "2001:4860:4860::8888"
           "2001:4860:4860::8844"
-          # cloudflare
+          "https://dns.google/dns-query"
           "1.1.1.1"
           "1.0.0.1"
           "2606:4700:4700::64"
           "2606:4700:4700::6400"
+          "https://dns.cloudflare.com/dns-query"
         ];
+        upstream_mode = "fastest_addr";
         filtering = {
           protection_enabled = true;
           filtering_enabled = true;
           parental_enabled = false;
-          safe_search.enabled = false;
+          safesearch_enabled = false;
+          safebrowsing_enabled = false;
         };
       };
       filters =
@@ -113,7 +129,8 @@ _:
     useDHCP = true;
     firewall = {
       enable = true;
-      allowedUDPPorts = [ 53 ];
+      allowedUDPPorts = [ config.services.adguardhome.settings.dns.port ];
+      allowedTCPPorts = [ config.services.adguardhome.settings.dns.port ];
     };
   };
 
