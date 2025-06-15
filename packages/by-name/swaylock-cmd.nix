9230e71fd0e41368c6f7cabe54a4d6371d17b05c
Paul Meyer <katexochen0@gmail.com>
2025-06-15 18:14:10 +0200

    swaylock-cmd: switch vid
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- packages/by-name/swaylock-cmd.nix
+++ packages/by-name/swaylock-cmd.nix
@@ -1,21 +1,26 @@
 {
   lib,
   writeShellApplication,
-  fetchurl,
+  fetchWithOras,
   swaylock-plugin,
   mpvpaper,
 }:
 
 let
-  nixcon-idle-loop-vid = fetchurl {
-    url = "https://github.com/nixcon/2024-intro-generators/raw/refs/heads/main/blender/idle-loop.webm";
-    hash = "sha256-xPp7rSRCIumG/FpZ2axTRL4g/6dfWVXXUEJvysBVurw=";
+  # nixcon-idle-loop-vid = fetchurl {
+  #   url = "https://github.com/nixcon/2024-intro-generators/raw/refs/heads/main/blender/idle-loop.webm";
+  #   hash = "sha256-xPp7rSRCIumG/FpZ2axTRL4g/6dfWVXXUEJvysBVurw=";
+  # };
+  spain-vid = fetchWithOras {
+    name = "spain-vid";
+    image = "ghcr.io/katexochen/ca-store";
+    sha256 = "c09449047285c4e421912abeaee8fa55947620c8acbfe19f7993206a0918a91f";
   };
 in
 
 writeShellApplication {
   name = "swaylock-cmd";
   text = ''
-    ${lib.getExe swaylock-plugin} -f --command "${mpvpaper}/bin/mpvpaper -o 'loop' '*' '${nixcon-idle-loop-vid}'"
+    ${lib.getExe swaylock-plugin} -f --command "${mpvpaper}/bin/mpvpaper --auto-pause -o 'loop --deinterlace=yes --no-audio' '*' '${spain-vid}'"
   '';
 }
--- packages/default.nix
+++ packages/default.nix
@@ -5,9 +5,9 @@
 #   directory = ./by-name;
 # }
 
-{
+rec {
   matrixc = pkgs.callPackage ./by-name/matrixc.nix { };
-  swaylock-cmd = pkgs.callPackage ./by-name/swaylock-cmd.nix { };
+  swaylock-cmd = pkgs.callPackage ./by-name/swaylock-cmd.nix { inherit fetchWithOras; };
   mkpasswordfile = pkgs.callPackage ./by-name/mkpasswordfile.nix { };
   nodeshell = pkgs.callPackage ./by-name/nodeshell.nix { };
   nm-setup-rub-eduroam = pkgs.callPackage ./by-name/nm-setup-rub-eduroam.nix { };
