e955bb5efa488eb5c8a393bd9409652709506dd8
Paul Meyer <katexochen0@gmail.com>
2025-06-24 21:45:42 +0200

    atuin: update config
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- home/shell/atuin.nix
+++ home/shell/atuin.nix
@@ -3,12 +3,13 @@ _: {
     enable = true;
     enableBashIntegration = true;
     settings = {
-      update_check = false;
       style = "compact";
       filter_mode_shell_up_key_binding = "session";
       inline_height = 20;
       show_tabs = false;
       show_preview = true;
+      enter_accept = true;
+      exit_mode = "return-query";
       history_filter = [
         "^exit$"
         "^reboot$"
@@ -18,6 +19,7 @@ _: {
         "^ls?$"
         "^clear$"
       ];
+      prefers_reduced_motion = true;
     };
   };
 }
