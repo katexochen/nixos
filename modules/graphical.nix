d9fb94a1c38853f9ad1c5471e452b45addbec8ce
Paul Meyer <katexochen0@gmail.com>
2025-08-15 14:48:07 +0200

    greetd: migration
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -39,11 +39,10 @@
     };
     greetd = {
       enable = true;
-      vt = 2;
       settings = {
         default_session = {
           command = ''
-            ${pkgs.greetd.tuigreet}/bin/tuigreet \
+            ${pkgs.tuigreet}/bin/tuigreet \
               --remember \
               --time \
               --asterisks \
