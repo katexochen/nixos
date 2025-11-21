6a14fe75dc1790227bfdfe32235f5cddb2250b94
Paul Meyer <katexochen0@gmail.com>
2025-11-21 15:11:00 +0100

    vscode: add amp
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- home/desktop/vscode.nix
+++ home/desktop/vscode.nix
@@ -30,6 +30,22 @@
         quicktype.quicktype
         redhat.vscode-yaml
         rust-lang.rust-analyzer
+        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+          mktplcRef = {
+            publisher = "sourcegraph";
+            name = "amp";
+            version = "0.0.1763727068";
+            hash = "sha256-rbL0i6JS/I59NiOlkt6v3GVea4vLyzN7BMFYkbDpWho=";
+          };
+
+          meta = {
+            description = "Amp is a frontier coding agent for your editor and terminal, built by Sourcegraph.";
+            downloadPage = "https://marketplace.visualstudio.com/items?itemName=sourcegraph.amp";
+            homepage = "https://ampcode.com/";
+            license = lib.licenses.unfree;
+            maintainers = [ lib.maintainers.katexochen ];
+          };
+        })
         streetsidesoftware.code-spell-checker
         tim-koehler.helm-intellisense
         timonwong.shellcheck
